511110a08358aee636d953487f19b30280c5c6ce
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-06-13 12:22:19 +0530

    themeSwitch correction

--- app/components/useThemeSwitcher.tsx
+++ app/components/useThemeSwitcher.tsx
@@ -1,24 +1,28 @@
 import { useEffect, useState, Dispatch, SetStateAction } from "react";
 
-type Theme = "" | "light" | "dark" ;
+type Theme = "light" | "dark";
 
 const useThemeSwitcher = (): [Theme, Dispatch<SetStateAction<Theme>>] => {
-  const [theme, setTheme] = useState<Theme>("");
+  const [theme, setTheme] = useState<Theme>("dark");
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
       const userPref = window.localStorage.getItem("theme");
       if (userPref) {
         setTheme(userPref === "dark" ? "dark" : "light");
       }
+    }
   }, []);
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
       window.localStorage.setItem("theme", theme);
       if (theme === "dark") {
         document.documentElement.classList.add("dark");
       } else {
         document.documentElement.classList.remove("dark");
       }
+    }
   }, [theme]);
 
   useEffect(() => {
@@ -26,9 +30,15 @@ const useThemeSwitcher = (): [Theme, Dispatch<SetStateAction<Theme>>] => {
       const check = e.matches ? "dark" : "light";
       setTheme(check);
     };
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    mediaQuery.addEventListener("change", handleThemeChange);
-    return () => mediaQuery.removeEventListener("change", handleThemeChange);
+
+    if (typeof window !== "undefined") {
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      mediaQuery.addListener(handleThemeChange);
+
+      return () => {
+        mediaQuery.removeListener(handleThemeChange);
+      };
+    }
   }, []);
 
   return [theme, setTheme];
