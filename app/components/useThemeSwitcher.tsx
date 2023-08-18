4e231cbe6b6cc00999627c532cbe10da8673c929
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-08-18 21:08:03 +0530

    update

--- app/components/useThemeSwitcher.tsx
+++ app/components/useThemeSwitcher.tsx
@@ -3,28 +3,33 @@ import { useEffect, useState, Dispatch, SetStateAction } from "react";
 type Theme = "light" | "dark";
 
 const useThemeSwitcher = (): [Theme, Dispatch<SetStateAction<Theme>>] => {
-  const [theme, setTheme] = useState<Theme>(() => {
-    // Get the user's stored theme from localStorage or use the system default
-    const storedTheme = localStorage.getItem("theme");
-    const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const getInitialTheme = (): Theme => {
+    const storedTheme = typeof window !== "undefined" ? localStorage.getItem("theme") : null;
+    const prefersDark = typeof window !== "undefined" && window.matchMedia("(prefers-color-scheme: dark)").matches;
     return storedTheme === "dark" || storedTheme === "light" ? storedTheme : (prefersDark ? "dark" : "light");
-  });
+  };
+
+  const [theme, setTheme] = useState<Theme>(getInitialTheme);
 
   useEffect(() => {
-    const handleThemeChange = (event: MediaQueryListEvent) => {
-      setTheme(event.matches ? "dark" : "light");
-    };
+    if (typeof window !== "undefined") {
+      const handleThemeChange = (event: MediaQueryListEvent) => {
+        setTheme(event.matches ? "dark" : "light");
+      };
 
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    mediaQuery.addEventListener("change", handleThemeChange);
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      mediaQuery.addEventListener("change", handleThemeChange);
 
-    return () => mediaQuery.removeEventListener("change", handleThemeChange);
+      return () => mediaQuery.removeEventListener("change", handleThemeChange);
+    }
   }, []);
 
   useEffect(() => {
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(theme);
-    localStorage.setItem("theme", theme);
+    if (typeof window !== "undefined") {
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add(theme);
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   return [theme, setTheme];
