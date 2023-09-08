c35f9de17b7859de9c758bdcf9eac5e837276209
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-09-08 16:22:30 +0530

    reverting icon

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,13 +5,9 @@ import chhatresh from "@/app/assets/chhatresh_khatri.svg";
 const Footer = () => {
   return (
     <footer className="flex flex-col lg:flex-row justify-between items-center w-full shadow-top bg-navbarBg dark:bg-darker py-2 px-2 xs:px-4 md:px-12 lg:px-20 mt-auto">
-      <div
-          title="Chhatresh Khatri"
-          aria-label="Chhatresh Khatri"
-          className="flex justify-center items-center"
-        >
-          <Image src={chhatresh} alt="Chhatresh Khatri" />
-        </div>
+      <div title="Chhatresh Khatri" aria-label="Chhatresh Khatri" className="flex justify-center items-center">
+        <Image src={chhatresh} alt="Chhatresh Khatri" />
+      </div>
       <div className="flex items-center justify-center text-center">
         &copy; 2023 All Rights Reserved by Chhatresh Khatri
       </div>
