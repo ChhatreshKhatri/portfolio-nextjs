06df3ed6f1c1857e25718d69e6512686a3bd2c93
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-09-28 14:48:37 +0530

    footer changes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,13 +5,15 @@ const Footer = () => {
   return (
     <footer className="flex flex-col lg:flex-row justify-between items-center w-full shadow-top bg-navbarBg dark:bg-darker py-2 px-3 xs:px-6 md:px-12 lg:px-20 mt-auto space-y-0.5">
       <div className="flex justify-center items-center" title="Chhatresh Khatri">
-        <ChhatreshKhatri className={`flex`} />
+        <ChhatreshKhatri className={`flex`} alt="Chhatresh Khatri Logo" />
       </div>
-      <div className="flex items-center justify-center text-center">&copy; 2023 All Rights Reserved</div>
-      <div className="flex items-center justify-center text-center">
-        &nbsp; Developed By &nbsp;<strong>Chhatresh Khatri</strong>
+      <div className="flex flex-col lg:flex-row items-center justify-center text-center">
+        <p>
+          Developed By <strong>Chhatresh Khatri&nbsp;</strong>
+        </p>
+        <p>&copy; 2023 All Rights Reserved</p>
       </div>
-      <Link href={`/privacy-policy`}>Privacy Policy</Link>
+      <Link href="/privacy-policy">Privacy Policy</Link>
     </footer>
   );
 };
