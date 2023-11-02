e04272f1fb2af40ed39fb9c42b5f725afde9ce4a
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-11-02 16:09:58 +0530

    faviconvisibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,9 +60,9 @@ export const viewport = { themeColor: "#191919" };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <FaviconVisibility />
       <body className={`${poppins.className} w-full min-h-screen flex flex-col bg-light text-darked dark:bg-dark dark:text-light`}>
         <ThemeProviderWrapper>
-          <FaviconVisibility />
           <NavBar />
           <main className="w-full h-full flex items-center flex-col justify-center font-medium px-2 xxs:px-3 xs:px-6 md:px-12 lg:px-20 py-20 gap-y-8">
             {children}
