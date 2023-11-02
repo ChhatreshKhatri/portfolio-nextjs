9a8eaf28e15e8993c8da8c807f0827a70f5bbc6d
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-11-02 15:04:16 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import NavBar from "../components/Navbar";
+import NavBar from "@/components/Navbar";
 import { Poppins } from "next/font/google";
 import ThemeProviderWrapper from "@/components/ThemeProviderWrapper";
-import FaviconVisibility from "../components/FaviconVisibility";
+import FaviconVisibility from "@/components/FaviconVisibility";
 import Footer from "@/components/Footer";
 import "./globals.css";
 const poppins = Poppins({ subsets: ["latin"], weight: ["500", "700"] });
