a0c5cff0418f6ce6e059f04af63e707f90bdf275
Chhatresh Khatri <chhatresh.khatri1002@gmail.com>
2024-11-24 22:23:45 +0530

    semantic structure + project tags

--- app/components/AboutData.tsx
+++ app/components/AboutData.tsx
@@ -9,8 +9,8 @@ interface Skill {
 }
 
 interface Data {
-  Content: { title: string; text: string };
-  Sections: {
+  content: { title: string; text: string };
+  sections: {
     title: string;
     skills: Skill[];
   }[];
@@ -31,34 +31,32 @@ export default function About() {
   }
   return (
     <>
-      <div className="relative group rounded-lg w-full text-justify p-3 xs:p-6 xs:px-10 py-6">
+      <section className="relative group rounded-lg w-full text-justify p-3 xs:p-6 xs:px-10 py-6">
         <div className="absolute -inset-0 rounded-lg bg-gradient-to-r from-blue2/50 to-green2/50 opacity-20 transition duration-500 group-hover:opacity-50"></div>
-        <h2 className={`relative text-2xl xxs:text-3xl md:text-4xl text-center`}>{data.Content.title}</h2>
-        <p className="relative text-lg xs:text-xl mt-4">{data.Content.text}</p>
-      </div>
-      <button className="rounded flex items-center justify-center bg-gradient-to-r from-blue1 to-yellow1 p-0.5">
+        <h2 className={`relative text-2xl xxs:text-3xl md:text-4xl text-center`}>{data.content.title}</h2>
+        <p className="relative text-lg xs:text-xl mt-4">{data.content.text}</p>
+      </section>
+      <section role="button" className="rounded flex items-center justify-center bg-gradient-to-r from-blue1 to-yellow1 p-0.5">
         <h2 className="flex justify-center items-center font-semibold tracking-wider text-xl xxs:text-2xl md:text-3xl p-1 md:px-2 bg-buttonBg dark:bg-darked rounded">Technology Stack</h2>
-      </button>
-      <div className="relative group rounded-lg w-full">
-        <div className="absolute -inset-0 rounded-lg bg-gradient-to-r from-blue2/50 to-green2/50 opacity-20 transition duration-500 group-hover:opacity-50"></div>
-        <div className="relative flex flex-col w-full p-3 xs:p-6 xs:px-10 py-6 gap-4">
-          {data.Sections.map((section, index) => (
-            <div key={index} className="flex flex-col gap-4">
+      </section>
+        {data.sections.map((section, index) => (
+          <section key={index} className="relative group rounded-lg w-full p-3">
+            <div className="absolute -inset-0 rounded-lg bg-gradient-to-r from-blue2/50 to-green2/50 opacity-20 transition duration-500 group-hover:opacity-50"></div>
+            <div  className="relative flex flex-col gap-4">
               <h3 className="text-2xl xxs:text-3xl md:text-4xl text-center">{section.title}</h3>
               <div className="flex flex-wrap items-center justify-center gap-2 xs:gap-4">
                 {section.skills.map((data, index) => (
-                  <div key={index} className={`items-center justify-center p-0.5 rounded bg-gradient-to-r from-yellow1 to-blue1 dark:from-blue1 dark:to-yellow1`}>
-                    <div className={`flex justify-center items-center py-1 px-2 gap-x-2 rounded whitespace-nowrap bg-buttonBg dark:bg-darker `}>
+                  <article key={index} role="button" title={data.name} className={`items-center justify-center p-0.5 rounded bg-gradient-to-r from-yellow1 to-blue1 dark:from-blue1 dark:to-yellow1`}>
+                    <div className={`flex justify-center items-center py-1 px-2 gap-x-2 rounded whitespace-nowrap bg-buttonBg dark:bg-darker`}>
                       <Image src={data.icon} alt={data.name} height={32} width={32} className="w-6 h-6 xs:w-8 xs:h-8" />
                       <p>{data.name}</p>
                     </div>
-                  </div>
+                  </article >
                 ))}
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </section>
+        ))}
     </>
   );
 }
--- app/components/ProjectsData.tsx
+++ app/components/ProjectsData.tsx
@@ -3,23 +3,23 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import LinkButton from "./LinkButton";
 import Loading from "../loading";
+
 interface Link {
-  Type: string;
-  Url: string;
-  Icon: string;
-  Pos: string;
+  type: string;
+  url: string;
+  icon: string;
 }
 interface Project {
-  Heading: string;
-  SubHeading: string;
-  Description: string;
-  Image: string;
-  Links: Link[];
-  Order: string;
+  heading: string;
+  subHeading: string;
+  description: string;
+  image: string;
+  links: Link[];
+  tags: { name: string; url: string }[];
 }
 interface Projects {
-  Content: { Title: string; Description: string };
-  Projects: Project[];
+  content: { title: string; description: string };
+  projects: Project[];
 }
 export default function ProjectsData() {
   const [data, setData] = useState<Projects | null>(null);
@@ -36,35 +36,42 @@ export default function ProjectsData() {
   }
   return (
     <>
-      {data.Projects.map((project, index) => (
-        <div key={index} className="group relative">
+      {data.projects.map((project, index) => (
+        <section key={index} className="group relative">
           <div className="absolute -inset-0 rounded-lg bg-gradient-to-r from-blue2/50 to-green2/50 opacity-20 transition duration-500 group-hover:opacity-50"></div>
           <div className="relative grid grid-cols-1 w-full gap-1 lg:grid-cols-2 rounded-xl p-3 xs:p-6">
-            <div className={`${project.Order == "odd" ? "" : "lg:order-2"}`}>
+            <article className={`${(index + 1) % 2 ? "" : "lg:order-2"}`}>
               {/* Content for the first column */}
               <div className="w-full h-full flex flex-col justify-center items-center xs:px-8 gap-y-4">
                 {/* Centered content in the first column */}
-                <h2 className="text-2xl xxs:text-3xl md:text-4xl xl:text-5xl font-semibold text-center">{project.Heading}</h2>
-                <h3 className="text-xl xl:text-3xl font-semibold text-center">{project.SubHeading}</h3>
-                <p className="text-lg xs:text-xl text-justify">{project.Description} </p>
-                <div className="flex flex-wrap items-center justify-center  gap-2 xxs:gap-4 text-md xs:text-xl">
-                  {project.Links.map((link, index) => (
-                    <LinkButton key={index} name={link.Type} link={link.Url} url={link.Icon} pos={link.Pos} />
+                <h2 className="text-2xl xxs:text-3xl md:text-4xl xl:text-5xl font-semibold text-center">{project.heading}</h2>
+                <h3 className="text-xl xl:text-3xl font-semibold text-center">{project.subHeading}</h3>
+                <p className="text-lg xs:text-xl text-justify">{project.description} </p>
+                <div className="flex flex-wrap items-center justify-center gap-2 xxs:gap-4 text-md xs:text-xl">
+                  {project.links.map((link, index) => (
+                    <LinkButton key={index} name={link.type} link={link.url} url={link.icon} pos={index % 2} />
+                  ))}
+                </div>
+                <div className="flex flex-wrap items-center justify-center gap-2 xxs:gap-4 text-md xs:text-xl">
+                  {project.tags?.map((tag, index) => (
+                    <div key={index} role="button" title={tag.name} className="flex justify-center items-center font-semibold tracking-wider text-xl xxs:text-2xl md:text-3xl py-1 px-2 bg-buttonBg dark:bg-darked rounded border-2 border-dark dark:border-light">
+                      <Image title={tag.name} src={tag.url} alt={tag.name + " alt"} width={24} height={24} className="w-6 h-6 xs:w-8 xs:h-8 text-white" />
+                    </div>
                   ))}
                 </div>
               </div>
-            </div>
-            <div className="flex justify-start items-center mt-6 lg:mt-0">
+            </article>
+            <aside className="flex justify-start items-center mt-6 lg:mt-0">
               {/* Content for the second column */}
               <div className="w-full h-full flex justify-center items-center relative">
                 {/* Centered content in the second column */}
-                <div className="w-full h-[300px] md:h-[350px] relative overflow-hidden rounded-2xl scroll-div xs:px-8">
-                  <Image title={project.Heading} priority src={project.Image} alt={`${project.Heading} Image`} width={480} height={480} className="w-full h-full object-cover object-top transition-all duration-4000 ease-in-out rounded-2xl hover:object-bottom" />
+                <div role="img" aria-label={`${project.heading}'s Image`} className="w-full h-[300px] md:h-[350px] relative overflow-hidden rounded-2xl scroll-div xs:px-8">
+                  <Image title={project.heading} priority src={project.image} alt={`${project.heading} Image`} width={480} height={480} className="w-full h-full object-cover object-top transition-all duration-4000 ease-in-out rounded-2xl hover:object-bottom" />
                 </div>
               </div>
-            </div>
+            </aside>
           </div>
-        </div>
+        </section>
       ))}
     </>
   );
